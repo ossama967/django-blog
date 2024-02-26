b6acffe75c2d7fc46bb73458e33b0c5dde7e60cf
ossama Aldawamenah <ossamaomar@gmail.com>
2024-02-26 21:54:58 +0000

    Adding the model view from Django into blog urls.py and views.py and adding the subdir blog/templates/blog and adding post_list.html file to it and pasting some html code in it

--- blog/views.py
+++ blog/views.py
@@ -5,6 +5,6 @@ from .models import Post
 # Create your views here.
 class PostList(generic.ListView):
     # model = Post
-    queryset = Post.objects.all()
-    template_name = "post_list.html"
+    queryset = Post.objects.filter(status=0)
+    # template_name = "post_list.html"
 
