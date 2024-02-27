5725a45bd1a312709276377bdcfd122fb4c71b38
ossama Aldawamenah <ossamaomar@gmail.com>
2024-02-27 16:28:21 +0000

    Adding the django-summernote, PostAdmin class and taking of the admin.site.register(Post)

--- blog/admin.py
+++ blog/admin.py
@@ -2,9 +2,9 @@ from django.contrib import admin
 from .models import Post, Comment
 from django_summernote.admin import SummernoteModelAdmin
 
+
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
-
     list_display = ('title', 'slug', 'status')
     search_fields = ['title']
     list_filter = ('status',)
