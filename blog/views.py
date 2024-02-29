3a1e78227b11cb93a3773b5f3b4e2066e2a8ad24
ossama Aldawamenah <ossamaomar@gmail.com>
2024-02-29 14:35:40 +0000

    added templates, installed whitenoise and ran collectstatic that created staticfiles directory and copied 193 files in its subdir and set DEBUG to False

--- blog/views.py
+++ blog/views.py
@@ -5,7 +5,7 @@ from .models import Post
 # Create your views here.
 class PostList(generic.ListView):
     # model = Post
-    queryset = Post.objects.filter(status=0)
+    queryset = Post.objects.filter(status=1)
     template_name = "blog/index.html"
     paginate_by = 6
     # template_name = "post_list.html"
