0e1e2db12dc567316c58ed8dfbd78aeb0655e3f1
ossama Aldawamenah <ossamaomar@gmail.com>
2024-03-02 19:43:34 +0000

    Added the collaboration model and its forms.py and changed the about.html

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
+from django.contrib import messages
 from .models import Post
+from .forms import CommentForm
+from django.contrib.auth.models import User
+
+def profile_page(request):
+    user = get_object_or_404(User, user=request.user)
+    comments = user.commenter.all()
 
 # Create your views here.
 class PostList(generic.ListView):
@@ -14,22 +21,37 @@ class PostList(generic.ListView):
 def post_detail(request, slug):
     """
     Display an individual :model:`blog.Post`.
-
     **Context**
-
     ``post``
         An instance of :model:`blog.Post`.
-
     **Template:**
-
     :template:`blog/post_detail.html`
     """
-
     queryset = Post.objects.filter(status=1)
     post = get_object_or_404(queryset, slug=slug)
-
-    return render(
-        request,
-        "blog/post_detail.html",
-        {"post": post},
+    comments = post.comments.all().order_by("-created_on")
+    comment_count = post.comments.filter(approved=True).count()
+    
+    if request.method == "POST":
+        print('Received a POST request')
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.author = request.user
+            comment.post = post
+            comment.save()
+            messages.add_message(
+                request, messages.SUCCESS,'Comment submitted and awaiting approval'
+                )
+    
+    comment_form = CommentForm()
+    print('About to render template')
+    return render(request,
+    "blog/post_detail.html",
+    {
+        "post": post,
+        "comments": comments,
+        "comment_count": comment_count,
+        "comment_form": comment_form,
+    },
     )
